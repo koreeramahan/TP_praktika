9328082739843470dedf39bc1aa209c184a8545c
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-10-11 11:04:17 +0300

    ветка под 3 блок

--- src/Block36.java
+++ src/Block36.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Block36 {
+
+    static void solutions (int a, int b, int c)
+    {
+
+    }
+    static void findZip (String str)
+    {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Решение квадратного уравнения. Введите a,b,затем c: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        solutions(a,b,c);
+        /////////////////////////////////////
+        System.out.println("Задание 2");
+        System.out.println("Позиция второго вхождения zip в строку. Введите строку:");
+
+
+    }
 }
