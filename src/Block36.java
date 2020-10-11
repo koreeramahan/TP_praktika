8d2bd297709c2835bada3578ad193a0a91d37039
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-10-11 11:28:59 +0300

    Задачи 1,2

--- src/Block36.java
+++ src/Block36.java
@@ -4,16 +4,23 @@ public class Block36 {
 
     static void solutions (int a, int b, int c)
     {
-
+        double D=b*b-4*a*c;
+        if (D>0) System.out.println("2 Два решения");
+        else if (D==0) System.out.println("1 Одно решение");
+        else System.out.println("0 Нет решений");
     }
     static void findZip (String str)
     {
-
+        int res=-1;
+        int index = str.indexOf("zip");
+        if (index == -1 || index == str.length() - 1) res = -1;
+        res = str.indexOf("zip", index + 1);
+        System.out.println(res);
     }
     public static void main(String[] args) {
         System.out.println("Задание 1");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Решение квадратного уравнения. Введите a,b,затем c: ");
+        System.out.println("Количество решений квадратного уравнения. Введите a,b,затем c: ");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
@@ -21,7 +28,7 @@ public class Block36 {
         /////////////////////////////////////
         System.out.println("Задание 2");
         System.out.println("Позиция второго вхождения zip в строку. Введите строку:");
-
-
+        String str = sc.next();
+        findZip(str);
     }
 }
