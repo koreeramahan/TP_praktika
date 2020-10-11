a0ca15a0c07771e340cc2664a1085d206c1c0cba
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-10-11 12:00:43 +0300

    Задачи 1,2,3,4,5

--- src/Block36.java
+++ src/Block36.java
@@ -15,6 +15,32 @@ public class Block36 {
         if (index == -1 || index == str.length() - 1) System.out.println("-1");
         System.out.println(str.indexOf("zip", index + 1));
     }
+    static void checkPerfect (int perfect){
+        int sum = 0;
+        for (int i = 1; i < perfect; i++)
+        {
+            if (perfect%i==0) sum+=i;
+        }
+        if (sum==perfect) System.out.println("true");
+        else System.out.println("false");
+    }
+    static void flipEndChars(String line)
+    {
+        if (line.length() < 2) System.out.println("Низя");
+        if (line.charAt(0) == line.charAt(line.length()-1)) System.out.println("Первый и последний символы совпадают");
+        System.out.println(line.charAt(line.length()-1) + line.substring(1, line.length()-1) + line.charAt(0));
+    }
+    static void isValidHexCode(String line)
+    {
+        if (line.length()!= 7 || line.charAt(0) != '#') System.out.println("false");
+        for (int i = 1; i < line.length(); i++)
+        {
+            if (!( Character.isDigit(line.charAt(i))
+                    || "ABCDEFabcdef".contains(Character.toString((line.charAt(i))))))
+                System.out.println("false");
+        }
+        System.out.println("true");
+    }
     public static void main(String[] args) {
         System.out.println("Задание 1");
         Scanner sc = new Scanner(System.in);
@@ -28,5 +54,20 @@ public class Block36 {
         System.out.println("Позиция второго вхождения zip в строку. Введите строку:");
         String str = sc.next();
         findZip(str);
+        /////////////////////////////////////
+        System.out.println("Задание 3");
+        System.out.println("Совершенное ли число. Введите число:");
+        int perfect = sc.nextInt();
+        checkPerfect(perfect);
+        /////////////////////////////////////
+        System.out.println("Задание 4");
+        System.out.println("Замена первого и последнего символа. Введите строку:");
+        String line = sc.next();
+        flipEndChars(line);
+        /////////////////////////////////////
+        System.out.println("Задание 5");
+        System.out.println("Является ли строка допустимым 16ным кодом. Введите строку:");
+        String line2 = sc.next();
+        isValidHexCode(line2);
     }
 }
