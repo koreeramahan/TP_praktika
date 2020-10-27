84752b20bda41d42f64459e70ced56166f6098d7
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-10-27 19:28:59 +0300

    создание 4 блока

---
+++ src/Block46.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Block46 {
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String c = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 2");
+        System.out.println("");
+        String str = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 3");
+        System.out.println("");
+        String str1 = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 4");
+        System.out.println("");
+
+        /////////////////////////////////////
+        System.out.println("Задание 5");
+        System.out.println("");
+        String ves = sc.next();
+        String rost = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 6");
+        System.out.println("");
+        int x = sc.nextInt();
+
+        /////////////////////////////////////
+        System.out.println("Задание 7");
+        System.out.println("");
+        String str3 = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 8");
+        System.out.println("");
+        String str4 = sc.next();
+        String str5 = sc.next();
+
+        /////////////////////////////////////
+        System.out.println("Задание 9");
+        System.out.println("");
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        /////////////////////////////////////
+        System.out.println("Задание 10");
+        System.out.println("");
+        String str6 = sc.next();
+
+
+    }
+
+}
