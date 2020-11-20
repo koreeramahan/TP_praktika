fcb2707324226bb21ad9a941af7108862ce34940
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-11-20 17:21:03 +0300

    5.3,5.4,5.5,5.6

--- src/Block56.java
+++ src/Block56.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Block56 {
 
@@ -19,7 +20,9 @@ public class Block56 {
     public static void sumDigProd(ArrayList<Integer> mas)
     {
         int sum=0;
+        //складываем введенные числа
         for (int i:mas) sum+=i;
+        //умножаем цифры числа, пока оно не станет <=9
         while (sum>9)
         {
             String sumStr = Integer.toString(sum);
@@ -28,6 +31,59 @@ public class Block56 {
                 sum*=Integer.parseInt(String.valueOf(sumStr.charAt(i)));
         }
         System.out.println(sum);
+    }
+
+    public static void sameVowelGroup(String words) {
+        String[] strArr = words.split(" ");
+        HashSet<Character> pattern = new HashSet<>(); //создает коллекцию, которая использует хеш-таблицу для хранения
+        //добавляем в хэш все гласные первого слова
+        for (int i=0; i<strArr[0].length(); i++) {
+            char ch = strArr[0].charAt(i);
+            if (ch == 'e' || ch == 'y' || ch == 'u' || ch == 'i' || ch == 'o' || ch == 'a')
+                pattern.add(ch);
+        }
+        //находим слова с такими же гласными
+        StringBuilder res = new StringBuilder(strArr[0]);
+        for (int i=1; i<strArr.length; i++) {
+            for (int j=0; j<strArr[i].length(); j++) {
+                char ch=strArr[i].charAt(j);
+                if (!pattern.contains(ch) && (ch == 'e' || ch == 'y' || ch == 'u' || ch == 'i' || ch == 'o' || ch == 'a')) break;
+                if (j==strArr[i].length()-1) {
+                    res.append(',');
+                    res.append(strArr[i]);
+                }
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static void validateCard(String code) {
+        if (code.length()<14 || code.length()>19) System.out.println("false"); //длина карты
+        char check = code.charAt(code.length()-1); //посл цифра
+        code=code.substring(0, code.length()-1); //шаг1. убираем посл цифру из номера карты
+        code=new StringBuilder(code).reverse().toString(); //шаг2. переворачиваем
+        StringBuilder doubleCode=new StringBuilder();
+        //шаг 3. удваиваем числа на нечетных местах
+        for (int i=0; i<code.length(); i++)
+        {
+            if (i%2==0)
+            {
+                int val=Integer.parseInt(String.valueOf(code.charAt(i)))*2;
+                if (val>9) val=val/10+val%10;
+                doubleCode.append(val);
+            }
+            else doubleCode.append(code.charAt(i));
+        }
+        int sum=0;
+        //шаг 4. складываем все цифры
+        for (int i=0; i<doubleCode.length(); i++)
+        {
+            sum+=Integer.parseInt(String.valueOf(doubleCode.charAt(i)));
+        }
+        //шаг 5. вычитаем из 10 последнюю цифру суммы
+        sum=10-sum%10;
+        if (check==Integer.toString(sum).charAt(0)) System.out.println("true");
+        else System.out.println("false");
     }
 
     public static void main(String[] args) {
@@ -43,11 +99,19 @@ public class Block56 {
         System.out.println("Задание 4. Введите числа:");
         String[] strArr = sc4.nextLine().split(" ");
         ArrayList<Integer> mas = new ArrayList<>(strArr.length);
-        for (int i=0; i<strArr.length; i++)
-        {
-            mas.add(Integer.parseInt(strArr[i]));
-        }
+        for (String s:strArr) mas.add(Integer.parseInt(s));
         sumDigProd(mas);
+        ////////////////////////////////////////
+        Scanner sc5  = new Scanner(System.in);
+        System.out.println("Задание 5. Выводятся слова, в которых гласные такие же как в первом.");
+        System.out.println("Введите последовательность слов:");
+        String words = sc5.nextLine();
+        sameVowelGroup(words);
+        ////////////////////////////////////////
+        Scanner sc6  = new Scanner(System.in);
+        System.out.println("Введите номер карты:");
+        String code = sc6.next();
+        validateCard(code);
     }
 
 }
