09c643a8cf05f82475efe8433d60b93528c8ca97
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-11-20 16:36:48 +0300

    5.3,5.4

--- src/Block56.java
+++ src/Block56.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Block56 {
 
@@ -15,15 +16,38 @@ public class Block56 {
         else System.out.println("false");
     }
 
+    public static void sumDigProd(ArrayList<Integer> mas)
+    {
+        int sum=0;
+        for (int i:mas) sum+=i;
+        while (sum>9)
+        {
+            String sumStr = Integer.toString(sum);
+            sum=1;
+            for (int i=0; i<sumStr.length(); i++)
+                sum*=Integer.parseInt(String.valueOf(sumStr.charAt(i)));
+        }
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
 
         Scanner sc3 = new Scanner(System.in);
-        System.out.println("Задание 3. Последнее слово содержит одни и те же гласные, значит строки рифмуются.");
+        System.out.println("Задание 3. Может ли быть завершена входная строка");
         System.out.println("Введите две строки:");
         String not = sc3.next();
         String full = sc3.next();
         canComplete(not, full);
-
+        ///////////////////////////////////////
+        Scanner sc4  = new Scanner(System.in);
+        System.out.println("Задание 4. Введите числа:");
+        String[] strArr = sc4.nextLine().split(" ");
+        ArrayList<Integer> mas = new ArrayList<>(strArr.length);
+        for (int i=0; i<strArr.length; i++)
+        {
+            mas.add(Integer.parseInt(strArr[i]));
+        }
+        sumDigProd(mas);
     }
 
 }
