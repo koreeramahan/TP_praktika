c3cef1186477870e2a496005c8c93613c3fba516
Олеся <36519002+koreeramahan@users.noreply.github.com>
2020-11-20 15:53:34 +0300

    5.3

---
+++ src/Block56.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Block56 {
+
+    public static void canComplete(String not, String full)
+    {
+        int j = 0;
+        for (int i = 0; i < full.length(); i++)
+        {
+            if (j >= not.length()) System.out.println("true");
+            char ch = full.charAt(i);
+            if (ch == not.charAt(j)) j++;
+        }
+        if (j == not.length()) System.out.println("true");
+        else System.out.println("false");
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc3 = new Scanner(System.in);
+        System.out.println("Задание 3. Последнее слово содержит одни и те же гласные, значит строки рифмуются.");
+        System.out.println("Введите две строки:");
+        String not = sc3.next();
+        String full = sc3.next();
+        canComplete(not, full);
+
+    }
+
+}
